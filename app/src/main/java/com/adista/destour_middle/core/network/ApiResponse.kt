53e70d010da7b4b1ce0ef7aca65e746494cab940
adistaufa17 <adistanurulfadilah01@gmail.com>
2025-03-26 03:52:07 +0700

    crocodic core -success

--- app/src/main/java/com/adista/destour_middle/core/network/ApiResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.adista.destour_middle.core.network
-
-import com.google.gson.annotations.SerializedName
-
-data class ApiResponse(
-    @SerializedName("status") val status: String,
-    @SerializedName("code") val code: Int,
-    @SerializedName("message") val message: String
-)
